9e1196137d7222360b4e5a8520b605b1792904e5
Brandon Fulk <bf@the42.com>
2013-02-20 05:13:15 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kopify::Application.config.secret_token = 'f0de13e372ace938ecdf7977047a6f16110d92f2789f1d6ba94bbd229d4c4428727765bbb5f68e152629cdfe2c0eecbf1151e5c3818826582bc9d6210693d7ff'
